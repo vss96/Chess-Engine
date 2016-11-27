4228cd2450bf45b873662c5708b343815e890a1c
Vikas <shettyvikas209@gmail.com>
2016-11-27 20:52:34 +0530

    Update pvp.cpp

--- pvp.cpp
+++ pvp.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
 using namespace std;
+typedef unsigned long long u64;
+class cboard
+{
+  private :
+  u64 WKnights;
+  u64 WPawns ;
+  u64 WRooks ;
+  u64 WBishops;
+  u64 WKing;
+  u64 WQueen;
+  u64 BKnights;
+  u64 BPawns ;
+  u64 BRooks ;
+  u64 BBishops;
+  u64 BKing;
+  u64 BQueen;
+}
 
 int main()
 {
