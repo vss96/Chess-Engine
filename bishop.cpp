4c4b784cf8533b9a88f4d2770301db0500979dc5
Vikas <shettyvikas209@gmail.com>
2017-04-13 13:58:58 +0800

    Minor changes
    Need to add an external function remove() , to check and update respective bitboard

--- bishop.cpp
+++ bishop.cpp
@@ -1,39 +1,58 @@
 bishop_moves( int rank1,char file1,int rank2,char file2,char type)
 {
 int index1=(rank1-1)*8 + (file1-96) -1;
-		int index2=(rank2-1)*8 + (file2-96) -1;
-		u64 a=pieces[index1];
-		u64 b=pieces[index2];
+int index2=(rank2-1)*8 + (file2-96) -1;
+u64 a=pieces[index1];
+u64 b=pieces[index2];
+u64 c=a;
     
     switch(type)
     {
     case 'w':
     case 'W':  
-		           if(file1<file2)
+		   if(file1<file2)
+		     {
+			if(rank1<rank2)
 			  {
-				if(rank1<rank2)
+			    while(a!=b)
+			     {
+			      a=a>>9;
+		              if(a&tboard!=0)
 				 {
-				   while(a!=b)
-				   {
-					 a=a>>9;
-					 if(a&tboard==0)
-					 {
-					    cout<<"Invalid move"<<endl;
-				        	break;
-					  }
-				   }
-				}
+			           cout<<"Invalid move"<<endl;
+			     	   break;
+			         }
+			     }
+			    if(a==b)
+			     {
+				WBishops=WBishops^c;
+				WBishops=WBishops|a;
+				    if(a&bboard==1)
+				    {
+					    remove(a,1);
+				    }
+			        total_board();
+			     }
+			  }
 				else if(rank1>rank2)
 				  {
 				     while(a!=b)
 					 { 
 					  a=a<<7;
-					  if(a&tboard==0)
+					  if(a&tboard!=0)
 					    {
 						 cout<<"Invalid move"<<endl;
 						 break;
 					    }
 					 }
+					if(a==b)
+					{
+						WBishops=WBishops^c;
+						WBishops=WBishops|a;
+						if(a&bboard==1)
+						{
+							remove(a,1);
+						}
 				  }
 				else
 				{
@@ -47,19 +66,21 @@ int index1=(rank1-1)*8 + (file1-96) -1;
 				 while(a!=b)
 				 {
 				   a=a>>7  ;
-				  if(a&tboard==0)
+				  if(a&tboard!=0)
 				   {
 				    cout<<"Invalid move"<<endl;
 				    break;
 			       	   }
 				 }
+					if(a==b)
+					{
 				}
 				 else if(rank1<rank2)
  		        	 {
 	          		   while(a!=b)
 				   {
 					   a=a<<9  ;
-					   if(a&tboard==0)
+					   if(a&tboard!=0)
 					   {
 					     cout<<"Invalid move"<<endl;
 				             break;
