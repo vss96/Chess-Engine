370e43225aa7ab721456d9db0d67084f8327d39d
Vikas <shettyvikas209@gmail.com>
2017-05-02 22:08:37 +0530

    Update queen.cpp

--- queen.cpp
+++ queen.cpp
@@ -1,1 +1,26 @@
 
+queen_moves(char file1,int rank1,char file2,int rank2,char type)
+{
+  switch(type)
+  {
+    case 'w' :
+    case 'W' :
+  if(abs(rank1-rank2)==int(abs(file1-file2)))
+    bishop_moves(file1,rank1,file2,rank2,'q');
+      else if((rank1-rank2)*(file1-file2)==0)
+        rook_moves(file1,rank1,file2,rank2,'q');
+      else
+      cout<<"Invalid move"<<endl;
+      break;
+    case 'b' :
+    case 'B' :
+     if(abs(rank1-rank2)==int(abs(file1-file2)))
+    bishop_moves(file1,rank1,file2,rank2,'Q');
+      else if((rank1-rank2)*(file1-file2)==0)
+        rook_moves(file1,rank1,file2,rank2,'Q');
+      else
+      cout<<"Invalid move"<<endl;
+      break;
+      default : cout<<"Invalid color"<<endl;
+  }
+}
