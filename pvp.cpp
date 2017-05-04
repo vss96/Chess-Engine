09a3fe414e0d00d51678a0c27660c4ede96956aa
Vikas <shettyvikas209@gmail.com>
2017-05-04 21:08:57 +0530

    Update pvp.cpp

--- pvp.cpp
+++ pvp.cpp
@@ -315,7 +315,10 @@ int index2=(rank2-1)*8 + (file2-96) -1;
 u64 a=pieces[index1];
 u64 b=pieces[index2];
 u64 c=a;
-	 check_piece(a,type,2);
+	 if(type=='w'||type=='W')
+	check_piece(a,type,2);
+	 else
+	 check_piece(a,type,4);
     if(abs(rank1-rank2)==abs(file1-file2))
     switch(type)
     {
@@ -953,17 +956,32 @@ break;
 	
 }
 }
-
-
-
-
-
-
-
-
-
 }
-
+queen_moves(char file1,int rank1,char file2,int rank2,char type)
+{
+  switch(type)
+  {
+    case 'w' :
+    case 'W' :
+  if(abs(rank1-rank2)==int(abs(file1-file2)))
+    bishop_moves(file1,rank1,file2,rank2,'q');
+      else if((rank1-rank2)*(file1-file2)==0)
+        rook_moves(file1,rank1,file2,rank2,'q');
+      else
+      cout<<"Invalid move"<<endl;
+      break;
+    case 'b' :
+    case 'B' :
+     if(abs(rank1-rank2)==int(abs(file1-file2)))
+    bishop_moves(file1,rank1,file2,rank2,'Q');
+      else if((rank1-rank2)*(file1-file2)==0)
+        rook_moves(file1,rank1,file2,rank2,'Q');
+      else
+      cout<<"Invalid move"<<endl;
+      break;
+      default : cout<<"Invalid color"<<endl;
+  }
+}
 };
 int main()
 {
